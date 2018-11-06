29d9058af290002998c35f76d33da6e54c7d420b
shlee2112 <shlee2112@gmail.com>
2018-11-06 10:13:24 -0500

    Add files via upload

--- draw.js
+++ draw.js
@@ -89,7 +89,47 @@ window.addEventListener("keydown", changeColor);
 
 
 
-canvas.ontouchstart = function(event){
+window.onload = function() {
+
+  document.ontouchmove = function(e){ e.preventDefault(); }
+
+  var canvas  = document.getElementById('main');
+  var canvastop = canvas.offsetTop
+
+  var context = canvas.getContext("2d");
+
+  var lastx;
+  var lasty;
+
+  context.strokeStyle = "#000000";
+  context.lineCap = 'round';
+  context.lineJoin = 'round';
+  context.lineWidth = 5;
+
+  function clear() {
+    context.fillStyle = "#ffffff";
+    context.rect(0, 0, 300, 300);
+    context.fill();
+  }
+
+  function dot(x,y) {
+    context.beginPath();
+    context.fillStyle = "#000000";
+    context.arc(x,y,1,0,Math.PI*2,true);
+    context.fill();
+    context.stroke();
+    context.closePath();
+  }
+
+  function line(fromx,fromy, tox,toy) {
+    context.beginPath();
+    context.moveTo(fromx, fromy);
+    context.lineTo(tox, toy);
+    context.stroke();
+    context.closePath();
+  }
+
+  canvas.ontouchstart = function(event){
     event.preventDefault();
 
     lastx = event.touches[0].clientX;
@@ -109,6 +149,13 @@ canvas.ontouchstart = function(event){
     lastx = newx;
     lasty = newy;
   }
+
+
+  var clearButton = document.getElementById('clear')
+  clearButton.onclick = clear
+
+  clear()
+}
 
 
 
